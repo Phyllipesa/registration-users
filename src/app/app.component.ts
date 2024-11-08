457c952b54cde4b74179557d1beccc359ad96f54
Phyllipe <phyllipe.sjc@hotmail.com>
2024-11-08 17:01:39 -0300

    Adicionando brazilianStateService e configurando metodo getStates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from './services/users.service';
 import { GendersService } from './services/genders.service';
+import { BrazilianStatesService } from './services/brazilian-states.service';
 
 @Component({
   selector: 'app-root',
@@ -10,23 +11,29 @@ import { GendersService } from './services/genders.service';
 export class AppComponent implements OnInit {
   usersList: any = [];
   gendersList: any = [];
+  statesList: any = [];
 
   constructor(
     private readonly _usersService: UsersService,
-    private readonly _gendersService: GendersService
+    private readonly _gendersService: GendersService,
+    private readonly _brazilianStatesService: BrazilianStatesService
   ) {}
 
   ngOnInit() {
     this.getUsers();
     this.getGenders();
+    this.getStates();
   }
-  
+
   private getUsers() {
     this._usersService.getUsers().subscribe((userListResponse) => this.usersList = userListResponse)
-    
   }
   
   private getGenders() {
     this._gendersService.getGenders().subscribe((gendersListResponse) => this.gendersList = gendersListResponse)
+  }
+
+  private getStates() {
+    this._brazilianStatesService.getStates().subscribe((statesListResponse) => this.statesList = statesListResponse)
   }
 }
