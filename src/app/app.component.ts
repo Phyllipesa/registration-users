0e096a6b0e8923bf09b44c13c213599713bb3b00
Phyllipe <phyllipe.sjc@hotmail.com>
2024-11-08 16:40:33 -0300

    Adicionando gendersService e configurando metodos ngOnInit, getUsers e getGenders

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from './services/users.service';
+import { GendersService } from './services/genders.service';
 
 @Component({
   selector: 'app-root',
@@ -8,12 +9,24 @@ import { UsersService } from './services/users.service';
 })
 export class AppComponent implements OnInit {
   usersList: any = [];
+  gendersList: any = [];
 
   constructor(
-    private readonly _userService: UsersService
+    private readonly _usersService: UsersService,
+    private readonly _gendersService: GendersService
   ) {}
 
   ngOnInit() {
-    this._userService.getUsers().subscribe((userListResponse) => this.usersList= userListResponse)
+    this.getUsers();
+    this.getGenders();
+  }
+  
+  private getUsers() {
+    this._usersService.getUsers().subscribe((userListResponse) => this.usersList = userListResponse)
+    
+  }
+  
+  private getGenders() {
+    this._gendersService.getGenders().subscribe((gendersListResponse) => this.gendersList = gendersListResponse)
   }
 }
