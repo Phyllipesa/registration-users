ceaf93f657356bc0bcbe43479f1af510eeb81b18
Phyllipe <phyllipe.sjc@hotmail.com>
2024-11-08 16:15:26 -0300

    Adicionando usersService e configurando ngInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from './services/users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'registration-users';
+export class AppComponent implements OnInit {
+  usersList: any = [];
+
+  constructor(
+    private readonly _userService: UsersService
+  ) {}
+
+  ngOnInit() {
+    this._userService.getUsers().subscribe((userListResponse) => this.usersList= userListResponse)
+  }
 }
