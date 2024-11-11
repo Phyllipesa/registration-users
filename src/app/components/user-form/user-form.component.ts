a45a9db9cf43ed3f07d4aa2db8b94e9f7ff01aa9
Phyllipe <phyllipe.sjc@hotmail.com>
2024-11-11 13:29:35 -0300

    Passando os Inputs para o UseFormComponent

--- src/app/components/user-form/user-form.component.ts
+++ src/app/components/user-form/user-form.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { GenresListResponse } from '../../types/genres-list-response';
+import { IUser } from '../../interfaces/user/user.interface';
+import { StatesListResponse } from '../../types/states-list-response';
 
 @Component({
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
   styleUrl: './user-form.component.scss'
 })
-export class UserFormComponent {
+export class UserFormComponent implements OnInit, OnChanges {
+  @Input() genresList: GenresListResponse = [];
+  @Input() statesList: StatesListResponse = [];
+  @Input() userSelected: IUser = {} as IUser;
 
+  ngOnInit() {
+    // console.log('ngOnInit');
+    // console.log('genreList', this.genresList);
+    // console.log('statesList', this.statesList);
+    // console.log('userSelected', this.userSelected);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    // console.log('ngOnChanges');
+    // console.log('genreList', this.genresList);
+    // console.log('statesList', this.statesList);
+    // console.log('userSelected', this.userSelected);
+  }
 }
