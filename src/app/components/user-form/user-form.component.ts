73890291049de83ddeedb7a24587a50872ca7f63
Phyllipe <phyllipe.sjc@hotmail.com>
2024-11-11 14:02:43 -0300

    Refatorando estrutura utilizando angularMaterial, Adicionando configurações para o campo Usuário.

--- src/app/components/user-form/user-form.component.ts
+++ src/app/components/user-form/user-form.component.ts
@@ -8,22 +8,12 @@ import { StatesListResponse } from '../../types/states-list-response';
   templateUrl: './user-form.component.html',
   styleUrl: './user-form.component.scss'
 })
-export class UserFormComponent implements OnInit, OnChanges {
+export class UserFormComponent implements OnChanges {
   @Input() genresList: GenresListResponse = [];
   @Input() statesList: StatesListResponse = [];
   @Input() userSelected: IUser = {} as IUser;
 
-  ngOnInit() {
-    // console.log('ngOnInit');
-    // console.log('genreList', this.genresList);
-    // console.log('statesList', this.statesList);
-    // console.log('userSelected', this.userSelected);
-  }
-
   ngOnChanges(changes: SimpleChanges) {
-    // console.log('ngOnChanges');
-    // console.log('genreList', this.genresList);
-    // console.log('statesList', this.statesList);
-    // console.log('userSelected', this.userSelected);
+    console.log('ngOnChanges');
   }
 }
