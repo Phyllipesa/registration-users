b676dfa45ff4fe409ba5065b25111923bdb779cd
Phyllipe <phyllipe.sjc@hotmail.com>
2024-11-11 14:22:29 -0300

    Atualização de imports do directivesModule

--- src/app/directives/directives.module.ts
+++ src/app/directives/directives.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
+import { EmailPatternValidatorDirective } from './email-pattern-validator.directive';
 
 
 
 @NgModule({
   declarations: [
-
+    EmailPatternValidatorDirective
   ],
   imports: [
-
   ],
   exports: [
-    
+    EmailPatternValidatorDirective
   ],
 })
 export class DirectivesModule { }
