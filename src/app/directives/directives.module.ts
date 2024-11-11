a9f0f8cf6d3e225fac469071401ff9d1e0fda7c6
Phyllipe <phyllipe.sjc@hotmail.com>
2024-11-11 16:36:41 -0300

    Atualização de imports do directivesModule

--- src/app/directives/directives.module.ts
+++ src/app/directives/directives.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { EmailPatternValidatorDirective } from './email-pattern-validator.directive';
 import { CredentialsValidatorDirective } from './credentials-validator.directive';
+import { PasswordStrengthValidatorDirective } from './password-strength-validator.directive';
 
 
 
 @NgModule({
   declarations: [
     EmailPatternValidatorDirective,
-    CredentialsValidatorDirective
+    CredentialsValidatorDirective,
+    PasswordStrengthValidatorDirective
   ],
   imports: [
   ],
   exports: [
     EmailPatternValidatorDirective,
-    CredentialsValidatorDirective
+    CredentialsValidatorDirective,
+    PasswordStrengthValidatorDirective
   ],
 })
 export class DirectivesModule { }
