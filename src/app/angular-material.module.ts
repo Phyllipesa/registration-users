0d912db285eb9bd2f248db089852932c385a3810
Phyllipe <phyllipe.sjc@hotmail.com>
2024-11-13 10:32:00 -0300

    Adicionando MatButtonModule e MatNativeDateModule ao angularMaterialModule

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from "@angular/core";
 import { MatCardModule } from "@angular/material/card";
 import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
 import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatNativeDateModule } from "@angular/material/core";
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatNativeDateModule } from "@angular/material/core";
 
 
 @NgModule({
     imports: [
         MatCardModule,
         MatInputModule,
+        MatButtonModule,
         MatFormFieldModule,
         MatNativeDateModule,
         MatDatepickerModule,
@@ -19,6 +21,7 @@ import { MatNativeDateModule } from "@angular/material/core";
     exports: [
         MatCardModule,
         MatInputModule,
+        MatButtonModule,
         MatFormFieldModule,
         MatNativeDateModule,
         MatDatepickerModule,
