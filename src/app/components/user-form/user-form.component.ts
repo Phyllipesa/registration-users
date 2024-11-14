a2d13505035a6f56a431a575894696746d5d07df
Phyllipe <phyllipe.sjc@hotmail.com>
2024-11-14 13:16:16 -0300

    Adicionando o array com o titulo das colunas da tabela

--- src/app/components/user-form/user-form.component.ts
+++ src/app/components/user-form/user-form.component.ts
@@ -19,6 +19,8 @@ export class UserFormComponent implements OnInit, OnChanges {
   maxDate: Date | null = null;
   dateValue: Date | null = null;
 
+  displayedColumns: string[] = ['title', 'band', 'genre', 'favorite'];
+
   @Input() genresList: GenresListResponse = [];
   @Input() statesList: StatesListResponse = [];
   @Input() userSelected: IUser = {} as IUser;
