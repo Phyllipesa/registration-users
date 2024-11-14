eb0e71a1f5ecaca7f959acd1b10becee64dabbc7
Phyllipe <phyllipe.sjc@hotmail.com>
2024-11-14 12:20:32 -0300

    Configurando a lógica do onDateChange

--- src/app/components/user-form/user-form.component.ts
+++ src/app/components/user-form/user-form.component.ts
@@ -4,6 +4,8 @@ import { GenresListResponse } from '../../types/genres-list-response';
 import { StatesListResponse } from '../../types/states-list-response';
 import { convertPtBrDateToDateObj } from '../../utils/convert-pt-br-date-to-date-obj';
 import { getPasswordStrengthValue } from '../../utils/get-password-strength-value';
+import { MatDatepickerInputEvent } from '@angular/material/datepicker';
+import { convertDateObjToPtBrDate } from '../../utils/convert-date-obj-to-pt-br-date';
 
 @Component({
   selector: 'app-user-form',
@@ -11,11 +13,11 @@ import { getPasswordStrengthValue } from '../../utils/get-password-strength-valu
   styleUrl: './user-form.component.scss'
 })
 export class UserFormComponent implements OnInit, OnChanges {
-passwordStrenghtValue = 0;
-minDate: Date | null = null;
-maxDate: Date | null = null;
-dateValue: Date | null = null;
 
+  passwordStrenghtValue = 0;
+  minDate: Date | null = null;
+  maxDate: Date | null = null;
+  dateValue: Date | null = null;
 
   @Input() genresList: GenresListResponse = [];
   @Input() statesList: StatesListResponse = [];
@@ -24,27 +26,32 @@ dateValue: Date | null = null;
   ngOnInit() {
     this.setMindAndMaxDate();
   }
-  
+
   ngOnChanges(changes: SimpleChanges) {
     const USER_CHANGED = changes['userSelected'];
-    
+
     if (USER_CHANGED) {
       this.setBirthDateToDatepicker(this.userSelected.birthDate);
       this.onPasswordChange(this.userSelected.password);
     }
   }
- 
+
   onPasswordChange(password: string) {
     this.passwordStrenghtValue = getPasswordStrengthValue(password);
   }
-  
+
+  onDateChange(event: MatDatepickerInputEvent<any, any>) {
+    if (!event.value) return;
+    this.userSelected.birthDate = convertDateObjToPtBrDate(event.value);
+  }
+
   private setMindAndMaxDate() {
     this.maxDate = new Date();
     this.minDate = new Date(
-      new Date().getFullYear() -100, 0, 1
+      new Date().getFullYear() - 100, 0, 1
     );
   }
-  
+
   private setBirthDateToDatepicker(birthDate: string) {
     this.dateValue = convertPtBrDateToDateObj(birthDate)
   }
