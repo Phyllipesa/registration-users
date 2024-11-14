596776f32bb4da53a3a09cf669f3da4fc552418e
Phyllipe <phyllipe.sjc@hotmail.com>
2024-11-14 13:16:39 -0300

    Adicionando MatTableModule e  MatDividerModulen ao angularMaterialModule

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 
-
 @NgModule({
     imports: [
         MatCardModule,
         MatInputModule,
+        MatTableModule,
         MatSelectModule,
         MatButtonModule,
+        MatDividerModule,
         MatFormFieldModule,
         MatNativeDateModule,
         MatDatepickerModule,
@@ -23,8 +26,10 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
     exports: [
         MatCardModule,
         MatInputModule,
+        MatTableModule,
         MatSelectModule,
         MatButtonModule,
+        MatDividerModule,
         MatFormFieldModule,
         MatNativeDateModule,
         MatDatepickerModule,
