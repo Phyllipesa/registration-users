6e001bae77e1cd0f40d62090406001ec3b90aadf
Phyllipe <phyllipe.sjc@hotmail.com>
2024-11-15 19:43:09 -0300

    Atualizando GenreDescriptionPipe ao PipesModule

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
+import { GenreDescriptionPipe } from './genre-description.pipe';
 
 
 
 @NgModule({
   declarations: [
-
+    GenreDescriptionPipe
   ],
   imports: [
 
   ],
   exports: [
-
+    GenreDescriptionPipe
   ]
 })
 export class PipesModule { }
